c6582efffd1d90b7f0e4e404bbf1a0a3ee48e5cc
Taro FURUKAWA <tarowin12@gmail.com>
2014-06-02 10:31:22 +0900

    Added vert.x latest 2.1 and latest 2.

---
+++ vertx21.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class VertX21 < Formula
+  homepage 'http://vertx.io/'
+  url 'http://dl.bintray.com/vertx/downloads/vert.x-2.1.tar.gz'
+  sha1 '77d4eff1ee042774a4c0e313a71db921d8ce12fa'
+
+  def install
+    rm_f Dir["bin/*.bat"]
+    libexec.install %w[bin client conf lib]
+    doc.install %w[api-docs]
+    bin.install_symlink "#{libexec}/bin/vertx"
+  end
+end
